6bf176c1247dd05000319d08fa7b49a49e1df3ad
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-05-30 21:22:02 +0200

    LatestView Content

--- src/views/LatestView.vue
+++ src/views/LatestView.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>bonjour</div>
+  <div class="mt-12 px-5 flex flex-col gap-20">
+    <NewsCards/>
+    <ReleasesCards/>
+    <NewsCards/>
+    <ReleasesCards/>
+  </div>
 </template>
 
 <script>
+import NewsCards from '../components/cards/NewsCards.vue'
+import ReleasesCards from '../components/cards/ReleasesCards.vue'
 export default {
-
+  components:{ NewsCards, ReleasesCards }
 }
 </script>
 

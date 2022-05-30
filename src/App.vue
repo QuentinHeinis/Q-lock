77727f5b0845e40fe3cf3c7dd01d67bf58ba4177
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-05-30 16:06:33 +0200

    modif header et ajout footer

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div class="flex w-full justify-between">
         <MenuAlt1Icon class="h-8 w-8" @click="MenuOpen=!MenuOpen" v-if="!MenuOpen"/>
         <XIcon class="h-8 w-8 absolute z-50" @click="MenuOpen=!MenuOpen" v-if="MenuOpen"/>
-        <MenuAlt1Icon class="h-8 w-8 stroke-white"/>
-        <MenuAlt1Icon class="h-8 w-8 stroke-white"/>
+        <Logo class="w-11 h-11"/>
+        <SearchIcon class="h-8 w-8"/>
     </div>
     <nav class="h-screen absolute w-screen bg-true-gray-350 left-0 top-0 -translate-x-full opacity-0" :class="MenuOpen && 'translate-x-0 opacity-100'">
         
@@ -27,14 +27,37 @@
         </ul>
     </nav>
 </header>
-<main class="-mt-16 pt-16 bg-true-gray-900 min-h-screen text-white relative -z-0">
+<main class="-mt-16 pb-28 pt-16 bg-true-gray-900 min-h-screen text-white relative -z-0">
     <RouterView/>
 </main>
+
+<footer class="bg-true-gray-900 flex flex-col w-full font-source-sans-pro">
+    <span class="w-10/12 h-[1px] bg-[#FAFAFA] mx-auto flex-none"></span>
+    <div class="w-full flex justify-between px-7 py-5">
+        <div class="flex flex-col">
+            <p class="text-white font-semibold">Legal</p>
+            <RouterLink to="/Mentions" class="text-true-gray-350 text-[10px]">Terms & conditions Privacy & cookie statement</RouterLink>
+        </div>
+        <div class="flex flex-col">
+            <p class="text-white font-semibold">Social</p>
+            <a href="" class="text-true-gray-350 text-[10px]">Youtube</a>
+            <a href="" class="text-true-gray-350 text-[10px]">Twitter</a>
+            <a href="" class="text-true-gray-350 text-[10px]">Facebook</a>
+            <a href="" class="text-true-gray-350 text-[10px]">Instagram</a>
+        </div>
+        <div class="flex flex-col">
+            <p class="text-white font-semibold">About</p>
+            <a href="" class="text-true-gray-350 text-[10px]">Jobs</a>
+            <a href="" class="text-true-gray-350 text-[10px]">Hardcore</a>
+        </div>
+    </div>
+</footer>
 </template>
 
 <script>
 import router from "./router"
-import {MenuAlt1Icon, XIcon} from "@heroicons/vue/outline"
+import {MenuAlt1Icon, XIcon, SearchIcon} from "@heroicons/vue/outline"
+import Logo from "./components/logo.vue"
 
 export default {
     data(){
@@ -42,7 +65,7 @@ export default {
             MenuOpen:false,
         }
     },
-    components:{MenuAlt1Icon, XIcon},
+    components:{ MenuAlt1Icon, XIcon, Logo, SearchIcon },
 }
 
 </script>

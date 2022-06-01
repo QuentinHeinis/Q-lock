f19c0f06e4f20125add9dc47f24ff1c6f2aa8546
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-06-01 16:40:43 +0200

    changement sur les tickets

--- src/views/Tickets/DetailsTicketView.vue
+++ src/views/Tickets/DetailsTicketView.vue
@@ -1,27 +1,65 @@
 <template>
   <div class="mt-12 px-5 flex flex-col gap-20 relative">
-    <h1 class="font-smythe text-2xl">Thursday</h1>
-    <Container class="flex flex-col gap-3">
-      <img src="/src/assets/Ticket.png" alt="" class="max-w-sm w-2/3 mx-auto">
-      <p>{{description}}</p>
+    <h1 class="font-smythe text-2xl">Tickets</h1>
+    <Container class="grid grid-cols-1 gap-3">
+      <h2 class="font-smythe text-lg text-center">{{ticket.jour}}</h2>
+      <div class="grid grid-cols-2">
+        <img src="https://firebasestorage.googleapis.com/v0/b/sae203-cf3d4.appspot.com/o/Contenu%2F6a7a1bed8b03132ee272b531fa9b5241.jpg?alt=media&token=791ebf74-cea6-4f33-bf6d-cc2bbd1dfae9" alt="image" class="max-w-sm w-2/3 mx-auto">
+        <div class="h-full flex flex-col justify-between">
+            <div>
+              <p>Ce Ticket vous donne accès :</p>
+              <p class="pl-4">- L'attraction principale</p>
+              <p class="pl-4">- La consommation( payante )</p>
+              <p class="pl-4">- Les toilettes</p>
+              <p>pour une personne uniquement et pendant le temps associer a ticket ( voir plus de details sur le ticket )</p>
+            </div>
+            <p class="font-smythe text-xl text-center">{{ticket.prix}} €</p>
+            <SolidBtn><RouterLink to="/">Acheter</RouterLink></SolidBtn>
+        </div>
+      </div>
     </Container>
-    <div class="flex justify-evenly w-full">
-        <SolidBtn><RouterLink to="/">Acheter</RouterLink></SolidBtn>
-        <OutlineBtn><RouterLink to="/Tickets">Retour</RouterLink></OutlineBtn>
-    </div>
+        <OutlineBtn class="mx-auto w-fit"><RouterLink to="/Tickets" >Retour</RouterLink></OutlineBtn>
   </div>
 </template>
 
 <script>
+import { 
+    getFirestore, 
+    collection, 
+    doc, 
+    getDoc,
+    updateDoc, 
+    onSnapshot, 
+    query,
+    orderBy
+    } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js'
 import Container from '../../components/Container.vue';
 import OutlineBtn from '../../components/buttons/outlineBtn.vue';
 import SolidBtn from '../../components/buttons/solidBtn.vue';
 export default {
     components: { Container, OutlineBtn, SolidBtn },
-    props:{
-      description:{ type:String,
-      default:"Fresh Friday is Defqon.1’s latest addition. A full-on festival day that rivals the magnitude of the Saturday with all stages, including the RED, opened all day!"
-      },
+    data(){
+        return{
+            ticket:{
+              jour:null,
+              prix:0
+            }
+        }
+    },
+    mounted(){
+      this.getTickets(this.$route.params.id);
+    },
+    methods:{
+      async getTickets(id){
+          const firestore = getFirestore();
+          const docRef = doc(firestore, "tickets", id);
+          this.refTicket = await getDoc(docRef);
+          if(this.refTicket.exists()){
+              this.ticket = this.refTicket.data();
+          }else{
+              this.console.log("latest inexistant");
+          }
+        },
     }
 }
 </script>
--- src/views/Tickets/TicketsView.vue
+++ src/views/Tickets/TicketsView.vue
@@ -4,7 +4,7 @@
     <Container class="flex flex-col gap-3">
       <h2 class="font-smythe text-xl">Tickets</h2>
       <div class="grid md:grid-cols-2 gap-3">
-        <LinkCards v-for="tick in ListeOffer" :key="tick.id" :text="tick.jour + ' - ' + tick.prix + ' €' " :image="'/src/assets/Ticket.png'" :chemin="'/DetailsTicket'"></LinkCards>
+        <LinkCards v-for="tick in ListeOffer" :key="tick.id" :text="tick.jour + ' - ' + tick.prix + ' €' " :image="'https://firebasestorage.googleapis.com/v0/b/sae203-cf3d4.appspot.com/o/Contenu%2FTicket.png?alt=media&token=59e29ea7-2dd7-4e18-92ef-5f491cae0844'" :chemin="{name: 'DetailsTicket', params:{id:tick.id}}" :alt="'Image du ticket'"></LinkCards>
       </div>
     </Container>
     <Background></Background>

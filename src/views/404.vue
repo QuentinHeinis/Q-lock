82d385d11ed6942004127501dd468782bf9d5c95
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-06-01 16:50:11 +0200

    mise en style 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,13 +1,5 @@
 <template>
-  vous vous etes échoué.e.s
+  <h1 class="font-smythe text-2xl text-center">Erreur 404</h1>
+  <img src="https://firebasestorage.googleapis.com/v0/b/sae203-cf3d4.appspot.com/o/Contenu%2FHumaaans.png?alt=media&token=cf63759c-c995-47d0-8119-d38d9355b64a" alt="image déco 404" class="mx-auto my-10">
+  <p class="text-center">Retourner à l'accueil ? :<RoutlerLink to="/" class="text-indigo-500"> Clique ici !</RoutlerLink> </p>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>

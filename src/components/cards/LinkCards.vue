68ba47152f1de66c28f95e5083bd1a594e666232
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-06-01 10:03:51 +0200

    modification conposant linkcard

--- src/components/cards/LinkCards.vue
+++ src/components/cards/LinkCards.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-[96px] w-full color rounded-default flex overflow-hidden font-source-sans-pro">
     <div class="flex-none w-[40%] max-w-[150px]">
-        <img :src="image" alt="" class="object-cover w-full h-full">
+        <img :src="image" :alt="alt" class="object-cover w-full h-full">
     </div>
     <div class="flex w-full justify-between items-center mb-3 px-3">
         <p class="text-black font-smythe text-lg">{{text}}</p>
@@ -26,8 +26,11 @@ export default {
             default:'/src/assets/Ticket.png'
         },
         chemin:{
-            type:String,
             default:'/'
+        },
+        alt:{
+            type:String,
+            default: 'image'
         }
     }
 }

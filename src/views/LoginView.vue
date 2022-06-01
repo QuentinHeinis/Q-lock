71be8f7bd3713f0fafd419b96f5fd3c1f330ec6c
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-06-01 20:20:21 +0200

    modif login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,10 +4,10 @@
             <form class="grid grid-cols-[minmax(max-content,8rem)_minmax(10rem,35rem)] gap-1" @submit.prevent="onCnx">
             <fieldset class="contents">
               <legend class="my-1 border-b-2 col-span-full">Se connecter</legend>
-              <label for="code-postal">Email :</label>
-              <input type="email" class="rounded-sm border-2 text-black" name="email" id="email" v-model="user.email" required>
-              <label for="ville">Password : </label>
-              <input class="rounded-sm border-2 text-black" name="password" id="password" type="password" v-model="user.password" required/>
+              <label >Email :</label>
+              <input type="email" class="rounded-sm border-2 text-black"  v-model="user.email" required>
+              <label >Password : </label>
+              <input class="rounded-sm border-2 text-black" type="password" v-model="user.password" required/>
               
                   <button type="submit">Se Connecter</button>
                   <button type="button" @click="onDcnx()">Se Déconnecter</button>
@@ -18,37 +18,64 @@
           <p class="w-full text-center py-3 bg-yellow-100 rounded-sm mt-5 text-black">{{message}}</p>
         </div>
         <div>
-            <form class="grid grid-cols-[minmax(max-content,8rem)_minmax(10rem,35rem)] gap-1">
+            <form class="grid grid-cols-[minmax(max-content,8rem)_minmax(10rem,35rem)] gap-1 " @submit.prevent="onCreate">
                 <fieldset class="contents">
                     <legend class="my-1 border-b-2 col-span-full">Créer un compte</legend>
-                    <label for="code-postal">Login :</label>
-                    <input type="text" class="rounded-sm border-2 text-black" name="login" id="login">
-                    <label for="code-postal">Email :</label>
-                    <input type="email" class="rounded-sm border-2 text-black" name="email" id="email">
-                    <label for="ville">Password : </label>
-                    <input class="rounded-sm border-2 text-black" name="password" id="password" type="password" />
-                    <button type="button" >Créer</button>     
+                    <label>Login :</label>
+                    <input type="text" class="rounded-sm border-2 text-black" v-model="user.login">
+                    <label>Email :</label>
+                    <input type="email" class="rounded-sm border-2 text-black" v-model="user.email">
+                    <label>Password : </label>
+                    <input type="password" class="rounded-sm border-2 text-black" v-model="user.password">
+                    <label>Répéter le Password : </label>
+                    <input class="rounded-sm border-2 text-black" type="password" v-model="password2"/>
+                    <button type="submit" >Créer</button>     
                 </fieldset>
             </form>
             
+        </div>
+        <div>
+            <p>Exemple compte admin :</p>
+            <p class="pl-5">email : toto@gmail.com</p>
+            <p class="pl-5">mdp : CompteAdmin</p>
+            <p>Exemple compte classique :</p>
+            <p class="pl-5">email : toto@yahoo.com</p>
+            <p class="pl-5">mdp : Compte</p>
         </div>
     </div>
 </template>
 
 <script>
 import {emitter} from '../main.js'
+import { 
+    getFirestore,   // Obtenir le Firestore
+    collection,     // Utiliser une collection de documents
+    doc,            // Obtenir un document par son id
+    getDocs,        // Obtenir la liste des documents d'une collection
+    getDoc,
+    addDoc,         // Ajouter un document à une collection
+    updateDoc,      // Mettre à jour un document dans une collection
+    deleteDoc,      // Supprimer un document d'une collection
+    onSnapshot,     // Demander une liste de documents d'une collection, en les synchronisant
+    query,          // Permet d'effectuer des requêtes sur Firestore
+    orderBy         // Permet de demander le tri d'une requête query
+    } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js'
 import {
     getAuth,
     signInWithEmailAndPassword,
-    signOut
+    signOut,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword 
 } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js'
 export default {
     data(){
         return{
             user:{
+                login:'',
                 email:'',
-                password:''
+                password:'',
             },
+            password2:'',
             message:'bonjour',
             type:'password'
         }
@@ -83,6 +110,42 @@ export default {
             .catch(error=>{
                 console.log('erreur deconnexion', error);
             })
+        },
+        onCreate(){
+            if(this.user.password===this.password2){
+                createUserWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
+                    .then((response) => {
+                        // Signed in 
+                        const user = response.user;
+                        // ...
+                    })
+                    .catch((error) => {
+                        console.log('erreur création', error);
+                        this.message = "erreur de création";
+                        // ..
+                })
+                
+                this.onCnx();
+                onAuthStateChanged(getAuth(), (user) => {
+                if (user) {
+                    const uid = user.uid;
+                    const firestore = getFirestore();
+                    const dbUser = collection(firestore, "users");
+                    const docRef = addDoc(dbUser,{
+                        uid: uid,
+                        admin: false,
+                        login: this.user.login
+                })
+                console.log('document créé avec le id : ', docRef.id); 
+                } else {
+                    console.log("user pas connecter")
+                }
+                });
+                
+            }else{
+                this.message = "password pas cohérent"
+                console.log('mdp mauvais')
+            }
         },
     },
 }

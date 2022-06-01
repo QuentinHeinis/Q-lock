c2d4c588cb22da15940e2d902d43580b0a6923fb
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-06-01 16:51:25 +0200

    fix probleme 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,5 @@
 <template>
   <h1 class="font-smythe text-2xl text-center">Erreur 404</h1>
   <img src="https://firebasestorage.googleapis.com/v0/b/sae203-cf3d4.appspot.com/o/Contenu%2FHumaaans.png?alt=media&token=cf63759c-c995-47d0-8119-d38d9355b64a" alt="image déco 404" class="mx-auto my-10">
-  <p class="text-center">Retourner à l'accueil ? :<RoutlerLink to="/" class="text-indigo-500"> Clique ici !</RoutlerLink> </p>
+  <p class="text-center">Retourner à l'accueil ? :<RouterLink to="/" class="text-indigo-500"> Clique ici !</RouterLink> </p>
 </template>
